33d54b976bbade9b88f87b4ec02d1591a9a99114
GuyAzulay <59178662+Guyazulay2@users.noreply.github.com>
2020-11-02 19:16:23 +0200

    Update WebRTC_Local.sh

--- WebRTC_Local.sh
+++ WebRTC_Local.sh
@@ -2,7 +2,9 @@
 
 echo "-- Install packages --" && sleep 3
 sudo apt-get install -y gstreamer1.0-tools gstreamer1.0-nice gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libgstreamer1.0-dev libglib2.0-dev libgstreamer-plugins-bad1.0-dev libsoup2.4-dev libjson-glib-dev
-sudo apt-get install ubuntu-restricted-extras ffmpeg
+sudo apt-get install ubuntu-restricted-extras ffmpeg -y 
+sudo apt-get install cmake ninja-build -y
+sudo apt-get install unzip -y
 echo "Done !" && sleep 2
 
 sudo mkdir /home/opts && cd /home/opts
@@ -23,11 +25,11 @@ sudo rm -rf WebRTCController
 
 sudo git clone https://github.com/dspip/WebRTCController 
 
-cd WebRTCController
+cd /home/WebRTCController
 
 VERSION=$(cat VERSION)
 
-sudo echo "'Version :'${VERSION}'Jenkins Build :'.${BUILD_NUMBER}" | sudo tee VERSION
+sudo echo -e "'Version :'${VERSION}\n'Jenkins Build :'.${BUILD_NUMBER}" | sudo tee VERSION
 
 sudo mkdir build 
 
